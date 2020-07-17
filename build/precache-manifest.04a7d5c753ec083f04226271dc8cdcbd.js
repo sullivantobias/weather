73bbd1cd068a8978a5df14eb5f1857d830b220a1
sullivan <stobias@sqli.com>
2020-07-17 22:53:42 +0200

    Release 1.0.0

---
+++ build/precache-manifest.04a7d5c753ec083f04226271dc8cdcbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fceafde7691f03b6636c9a8560f3b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ecc761aed4657a241e7",
+    "url": "/static/css/main.6d3fe61c.chunk.css"
+  },
+  {
+    "revision": "7ad373a901158081227f",
+    "url": "/static/js/2.0863974d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0863974d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecc761aed4657a241e7",
+    "url": "/static/js/main.b9eed03e.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
